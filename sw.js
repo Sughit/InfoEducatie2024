e09fa5e60b6cb667ca8d588edbf6e3540fc7fd86
Sughit <andreisorici2007@gmail.com>
2024-04-21 13:14:12 +0300

    nu stiu daca merge

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "./style.css", "./mecanica.html", "./termodinamica.html", "./electromecanica.html", "./optica.html", "./logos/logo192.png", "./logos/logo512.png"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e =>{
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
