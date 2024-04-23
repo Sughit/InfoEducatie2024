3e0af5f9e62d8ec37d2546d7b9decd0b6613fab0
Sughit <andreisorici2007@gmail.com>
2024-04-23 09:05:42 +0300

    am uitat asta

--- testLogic/testMecanica.js
+++ testLogic/testMecanica.js
@@ -1,15 +1,36 @@
 var questions = [
     {
+        nr: 1,
         prompt: "Test nr 1",
-        a: "d"
+        a: "d",
+        options: [
+            "a",
+            "b",
+            "c",
+            "d"
+        ]
     },
     {
+        nr: 2,
         prompt: "Test nr 2",
-        a: "a"
+        a: "a",
+        options: [
+            "a",
+            "b",
+            "c",
+            "d"
+        ]
     },
     {
+        nr: 3,
         prompt: "Test nr 3",
-        a: "b"
+        a: "b",
+        options: [
+            "a",
+            "b",
+            "c",
+            "d"
+        ]
     }
 ]
 var score = 0;
