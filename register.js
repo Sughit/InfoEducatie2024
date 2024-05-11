88f061dfbedab4d7f0cc18a0ab517531e2b5eaec
Sughit <andreisorici2007@gmail.com>
2024-05-11 19:36:22 +0300

    a mers un moment

---
+++ register.js
@@ -0,0 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApT-gHPS0DnfuV1n9BtstXScuPuNoBnrE",
+  authDomain: "fizix-57ec5.firebaseapp.com",
+  projectId: "fizix-57ec5",
+  storageBucket: "fizix-57ec5.appspot.com",
+  messagingSenderId: "443770625652",
+  appId: "1:443770625652:web:5a88975649d8121ec8fb21"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+const register = document.getElementById('register');
+register.addEventListener('click', function(event){
+    event.preventDefault();
+    
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        alert('Creating an Account');
+        window.location.href = "index.html";
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+        // ..
+    });
+})
