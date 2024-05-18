57226ae433bcafdebb1d055dc3d7cd039bd48bb7
emanuel5867 <130173546+emanuel5867@users.noreply.github.com>
2024-05-19 01:02:10 +0300

    .

--- testLogic/questions.js
+++ testLogic/questions.js
@@ -9,7 +9,7 @@ let questions1 = [
       'v<sup>2</sup> = v<sub>0</sub><sup>2</sup> + 2a(x - x<sub>0</sub>)',
       'v<sup>2</sup> + v<sub>0</sub><sup>2</sup> = 2ax<sub>0</sub>'
     ],
-    tag: "teorieMecanica.html#1"
+    tag: "./teorieMecanica.html#1"
   },
     {
     numb: 2,
@@ -21,7 +21,7 @@ let questions1 = [
       'm / s',
       'm ∙ s'
     ],
-    tag: "teorieMecanica.html#2"
+    tag: "./teorieMecanica.html#2"
   },
     {
     numb: 3,
