88ce359e4f9f60297c10561e2ee2bb1db3f01d0c
emanuel5867 <130173546+emanuel5867@users.noreply.github.com>
2024-05-20 03:38:59 +0300

    dadada

--- languages/langIndex.js
+++ languages/langIndex.js
@@ -341,62 +341,61 @@ function loadLanguages(attr)
         {
             var test = document.getElementById('1');
             test.id = 'highlight'
-            console.log(test);
             window.location.href = "/teorieMecanica.html#highlight";
         }
         else if(hash == "#2")
         {
             var test = document.getElementById('2');
             test.id = 'highlight'
-            console.log(test);
+            window.location.href = "/teorieMecanica.html#highlight";
         }
         else if(hash == "#3")
         {
             var test = document.getElementById('3');
             test.id = 'highlight'
-            console.log(test);
+            window.location.href = "/teorieMecanica.html#highlight";
         }
         else if(hash == "#4")
         {
             var test = document.getElementById('4');
             test.id = 'highlight'
-            console.log(test);
+            window.location.href = "/teorieMecanica.html#highlight";
         }
         else if(hash == "#5")
         {
             var test = document.getElementById('5');
             test.id = 'highlight'
-            console.log(test);
+            window.location.href = "/teorieMecanica.html#highlight";
         }
         else if(hash == "#6")
         {
             var test = document.getElementById('6');
             test.id = 'highlight'
-            console.log(test);
+            window.location.href = "/teorieMecanica.html#highlight";
         }
         else if(hash == "#7")
         {
             var test = document.getElementById('7');
             test.id = 'highlight'
-            console.log(test);
+            window.location.href = "/teorieMecanica.html#highlight";
         }
         else if(hash == "#8")
         {
             var test = document.getElementById('8');
             test.id = 'highlight'
-            console.log(test);
+            window.location.href = "/teorieMecanica.html#highlight";
         }
         else if(hash == "#9")
         {
             var test = document.getElementById('9');
             test.id = 'highlight'
-            console.log(test);
+            window.location.href = "/teorieMecanica.html#highlight";
         }
         else if(hash == "#10")
         {
             var test = document.getElementById('10');
             test.id = 'highlight'
-            console.log(test);
+            window.location.href = "/teorieMecanica.html#highlight";
         }
     }
 
